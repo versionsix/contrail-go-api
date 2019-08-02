61d134ded1b96ae7676cfb5df56add12cacef54c
xfx <xenefix@protonmail.com>
2019-08-02 17:09:11 +0200

    first commit

---
+++ types/namespace_value.go
@@ -0,0 +1,29 @@
+//
+// Automatically generated. DO NOT EDIT.
+//
+
+package types
+
+type AutonomousSystemsType struct {
+	Asn []int `json:"asn,omitempty"`
+}
+
+func (obj *AutonomousSystemsType) AddAsn(value int) {
+        obj.Asn = append(obj.Asn, value)
+}
+
+type NamespaceValue struct {
+	Ipv4Cidr *SubnetListType `json:"ipv4_cidr,omitempty"`
+	Asn *AutonomousSystemsType `json:"asn,omitempty"`
+	MacAddr *MacAddressesType `json:"mac_addr,omitempty"`
+	AsnRanges []AsnRangeType `json:"asn_ranges,omitempty"`
+	SerialNums []string `json:"serial_nums,omitempty"`
+}
+
+func (obj *NamespaceValue) AddAsnRanges(value *AsnRangeType) {
+        obj.AsnRanges = append(obj.AsnRanges, *value)
+}
+
+func (obj *NamespaceValue) AddSerialNums(value string) {
+        obj.SerialNums = append(obj.SerialNums, value)
+}
